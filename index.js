2267ae1492e37ea8af99ed6a0244eac4a56c99f7
Vanessa Martinez <vmar76@gmail.com>
2020-01-28 19:32:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,5 +16,6 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if(string===string.toLowerCase())
-  return string("I can't hear you!");
+  console.log("I can't hear you!");
+  return string;
 }
